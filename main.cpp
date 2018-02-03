43eee02fa0d18ef2bfba9e7ae38ed18789e0a9ec
cyanic-selkie <oskarovac.sven@gmail.com>
2018-02-03 18:01:12 +0100

    Fixes bugs

--- main.cpp
+++ main.cpp
@@ -2,51 +2,20 @@
 
 int main(){
     float height, tb_radius, mid_radius, result, x;
-    height = 20;
+    height = 1;
     tb_radius = 1;
     mid_radius = 0;
-    x = rand () % height
+    x = 1;
     if(mid_radius != 0){
         /*result = 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius );*/
-        result = ( 3.14 * x / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius ) ) / ( 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius ))) * 100 ;
-                       } 
-    else{
+        result = ( 3.14 * x / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius ) ) / ( 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius )) * 100 ;
+    }else{
         /*result = 3.14 * tb_radius * tb_radius * height ;  */
         result = ( ( 3.14 * tb_radius * tb_radius * x ) / ( 3.14 * tb_radius * tb_radius * height ) ) * 100;         
-        }
-    std::cout << x << std::endl;    
-    std::cout << result << std::endl;
-
-
-     return 0;
-}
-   
-
-/*
+    }
     
-        if(nesto == nesto){
-            bla bla
-        }else{
-            bla bla
-        }
+    std::cout << x << std::endl;    
+    std::cout << result << "%" << std::endl;
 
-        Ako je samo jedna linija onda ne treba {}
-
-        if(nesto == nesto)
-            bla bla
-        else
-            bla bla
-
-        kad imas nesto kao
-        
-        if(){
-
-        }
-
-        nikad ne stavljaj prvu { u drugu liniju npr.
-
-        if()
-        {
-            
-        }
-*/
+    return 0;
+}
