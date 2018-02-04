00d835b0189a7936a8d328c9782325a5507ac64c
CodeHackle <jukicpetar13@gmail.com>
2018-02-04 14:39:41 +0100

    Added menu

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include "windows.h"
 
 int main(){
+    int option = 0;
     float height, tb_radius, mid_radius, result, x;
-    height = 1;
+    /*height = 1;
     tb_radius = 1;
     mid_radius = 0;
-    x = 1;
-    if(mid_radius != 0){
-        /*result = 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius );*/
-        result = ( 3.14 * x / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius ) ) / ( 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius )) * 100 ;
-    }else{
-        /*result = 3.14 * tb_radius * tb_radius * height ;  */
-        result = ( ( 3.14 * tb_radius * tb_radius * x ) / ( 3.14 * tb_radius * tb_radius * height ) ) * 100;         
-    }
-    
-    std::cout << x << std::endl;    
-    std::cout << result << "%" << std::endl;
+    x = 0.5;*/
+    do {
+        std::cout << " Meni:" << std::endl;
+        std::cout << "1. Parametri bacve " << std::endl;
+        std::cout << "2. Popunjenost bacve " << std::endl;
+        cin>> option;
+            switch(option){
+                case 1:
+                    std::cout<< "Parametri bacve:"<< std::endl; 
+                    std::cout<< "Visina:"<< height << std::endl;
+                    cin>> height; 
+                    std::cout<< "Gorni/Donji radius:"<< tb_radius << std::endl;
+                    cin>> tb_radius;
+                    std::cout<< "Sredisnji radius:"<< mid_radius << std::endl;
+                    cin>> mid_radius;
+                    break;
+                case 2:    
 
-    return 0;
+                    if(mid_radius != 0){
+        /*result = 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius );*/
+                        result = ( 3.14 * x / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius ) ) / ( 3.14 * height / 12 * (2 * mid_radius * mid_radius + tb_radius * tb_radius )) * 100 ;
+                    }else{
+        /*result = 3.14 * tb_radius * tb_radius * height ;  */
+                        result = ( ( 3.14 * tb_radius * tb_radius * x ) / ( 3.14 * tb_radius * tb_radius * height ) ) * 100;         
+                         }
+    
+                 std::cout << x << std::endl;    
+                 std::cout << result << "%" << std::endl;    
+
+                 return 0;
+                        }
+        }                 
+
 }
